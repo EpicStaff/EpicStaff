b1335f79a59b40c8a7223673226c0fad29ddb9ed
KarolinaKudrytskaHYS <karolina.kudrytska@hys-enterprise.com>
2026-06-22 17:10:41 +0300

    fix(EST-3140): logs removed

--- frontend/src/app/pages/chats-page/libs/openai/api.js
+++ frontend/src/app/pages/chats-page/libs/openai/api.js
@@ -4,14 +4,15 @@ import { RealtimeUtils } from "./utils.js";
 export class RealtimeAPI extends RealtimeEventHandler {
   /**
    * Create a new RealtimeAPI instance
-   * @param {{url?: string, apiKey?: string, dangerouslyAllowAPIKeyInBrowser?: boolean, debug?: boolean}} [settings]
+   * @param {{url?: string, apiKey?: string, connectionKey?: string, dangerouslyAllowAPIKeyInBrowser?: boolean, debug?: boolean}} [settings]
    * @returns {RealtimeAPI}
    */
-  constructor({ url, apiKey, dangerouslyAllowAPIKeyInBrowser, debug } = {}) {
+  constructor({ url, apiKey, connectionKey, dangerouslyAllowAPIKeyInBrowser, debug } = {}) {
     super();
     this.defaultUrl = "wss://api.openai.com/v1/realtime";
     this.url = url || this.defaultUrl;
     this.apiKey = apiKey || null;
+    this.connectionKey = connectionKey || null;
     this.debug = !!debug;
     this.ws = null;
     if (globalThis.document && this.apiKey) {
@@ -74,7 +75,7 @@ export class RealtimeAPI extends RealtimeEventHandler {
       }
       const WebSocket = globalThis.WebSocket;
       const key = localStorage.getItem("connectionKey"); // Retrieve connection key
-      console.log(key);
+
       const ws = new WebSocket(
         `${this.url}${key ? `?connection_key=${key}` : ""}`,
         [
