fce9c1f8937e78c605fd640c7dea5a479f88191f
ArturDrohomeretskyiHYS <artur.drohomeretskyi@hys-enterprise.com>
2026-06-26 20:25:57 +0300

    fix(EST-2423-BE): sub:EST-2938, org-scope hybrid model/python-tool imports (built-in shared, custom per-org)

--- src/django_app/tables/import_export/strategies/python_tools.py
+++ src/django_app/tables/import_export/strategies/python_tools.py
@@ -1,5 +1,7 @@
 from copy import deepcopy
 
+from django.db.models import Q
+
 from tables.models import PythonCode, PythonCodeTool
 from tables.import_export.strategies.base import EntityImportExportStrategy
 from tables.import_export.serializers.python_tools import (
@@ -33,15 +35,23 @@ class PythonCodeToolStrategy(EntityImportExportStrategy):
     def export_entity(self, instance: PythonCodeTool) -> dict:
         return self.serializer_class(instance).data
 
+    def get_org_scope_q(self, org_id: int) -> Q:
+        if org_id is None:
+            return Q()
+        return Q(built_in=True) | Q(org_id=org_id)
+
     def create_entity(
         self, data: dict, id_mapper: IDMapper, **kwargs
     ) -> PythonCodeTool:
+        org_id = kwargs.get("org_id")
         python_code_data = data.pop("python_code", {})
         python_tool_config_data = data.pop("python_code_tool_config", [])
         python_tool_config_fields_data = data.pop("python_code_tool_config_fields", [])
 
         if "name" in data:
-            existing_names = PythonCodeTool.objects.values_list("name", flat=True)
+            existing_names = PythonCodeTool.objects.filter(org_id=org_id).values_list(
+                "name", flat=True
+            )
             data["name"] = ensure_unique_identifier(
                 base_name=data["name"],
                 existing_names=existing_names,
@@ -50,19 +60,26 @@ class PythonCodeToolStrategy(EntityImportExportStrategy):
         python_code = self._create_python_code(python_code_data)
 
         serializer = self.serializer_class(
-            data={**data, "python_code_id": python_code.id}
+            data={
+                **data,
+                "python_code_id": python_code.id,
+                "org": org_id,
+                "built_in": False,
+            }
         )
         serializer.is_valid(raise_exception=True)
         python_code_tool = serializer.save()
 
-        self._create_python_tool_config(python_code_tool, python_tool_config_data)
+        self._create_python_tool_config(
+            python_code_tool, python_tool_config_data, org_id
+        )
         self._create_python_tool_config_fields(
             python_code_tool, python_tool_config_fields_data
         )
 
         return python_code_tool
 
-    def find_existing(self, data, id_mapper):
+    def find_existing(self, data, id_mapper, org_id: int = None):
         data_copy = deepcopy(data)
         data_copy.pop("id", None)
         data_copy.pop("python_code_tool_config", None)
@@ -71,9 +88,11 @@ class PythonCodeToolStrategy(EntityImportExportStrategy):
         python_code_data = data_copy.pop("python_code", None)
 
         filters, null_filters = create_filters(data_copy)
-        existing_python_tool = PythonCodeTool.objects.filter(
-            **filters, **null_filters
-        ).first()
+        existing_python_tool = (
+            PythonCodeTool.objects.filter(**filters, **null_filters)
+            .filter(self.get_org_scope_q(org_id))
+            .first()
+        )
 
         if not existing_python_tool:
             return None
@@ -91,11 +110,13 @@ class PythonCodeToolStrategy(EntityImportExportStrategy):
         return serializer.save()
 
     def _create_python_tool_config(
-        self, tool: PythonCodeTool, python_tool_config_data: dict
+        self, tool: PythonCodeTool, python_tool_config_data: dict, org_id
     ):
         for tool_config_data in python_tool_config_data:
             tool_config_data["tool_id"] = tool.id
-            serializer = PythonCodeToolConfigImportSerializer(data=tool_config_data)
+            serializer = PythonCodeToolConfigImportSerializer(
+                data={**tool_config_data, "org": org_id}
+            )
             serializer.is_valid(raise_exception=True)
             serializer.save()
 
