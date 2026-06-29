6010823770be09265a0d87c8f8658f4a5da5fc63
BogdanTimchenkoHYS <bogdan-timchenko@hys-enterprise.com>
2026-06-29 17:17:18 +0300

    ref(EST-3154-BE): simplify indexing error classifier

--- src/knowledge_NEW/services/indexing_error_classifier.py
+++ src/knowledge_NEW/services/indexing_error_classifier.py
@@ -1,7 +1,12 @@
 """Map indexing exceptions to a persisted (error_code, error_message) pair."""
 
 from enums import DocumentErrorCode
-from errors import FileTextExtractingError, ChunkingError, EmbeddingError
+from errors import (
+    FileTextExtractingError,
+    ChunkingError,
+    EmbeddingError,
+    NoPreviewChunksProducedError,
+)
 from utils import format_error_message
 
 
@@ -9,21 +14,12 @@ class IndexingErrorClassifier:
     """Classify indexing failures by the phase they occurred in."""
 
     @classmethod
-    def for_chunking(cls, exc: BaseException) -> tuple[DocumentErrorCode, str]:
-        return DocumentErrorCode.CHUNKING_FAILED, format_error_message(exc)
-
-    @classmethod
-    def for_embedding(cls, exc: BaseException) -> tuple[DocumentErrorCode, str]:
-        return DocumentErrorCode.EMBEDDING_FAILED, format_error_message(exc)
-
-    @classmethod
-    def for_unknown(cls, exc: BaseException) -> tuple[DocumentErrorCode, str]:
-        return DocumentErrorCode.UNKNOWN, format_error_message(exc)
-
-    @classmethod
     def classify(cls, exc: BaseException) -> tuple[DocumentErrorCode, str]:
-        if isinstance(exc, (FileTextExtractingError, ChunkingError)):
-            return cls.for_chunking(exc)
-        if isinstance(exc, EmbeddingError):
-            return cls.for_embedding(exc)
-        return cls.for_unknown(exc)
+        if isinstance(exc, (FileTextExtractingError, ChunkingError, NoPreviewChunksProducedError)):
+            error_code = DocumentErrorCode.CHUNKING_FAILED
+        elif isinstance(exc, EmbeddingError):
+            error_code = DocumentErrorCode.EMBEDDING_FAILED
+        else:
+            error_code = DocumentErrorCode.UNKNOWN
+
+        return error_code, format_error_message(exc)
