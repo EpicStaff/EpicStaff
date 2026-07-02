5e34d022586930c0a2aa847a029ce2d1223e5e94
andrey.krasnoyarskiy <andrey.krasnoyarskiy@hys-enterprise.com>
2026-07-02 09:30:57 +0300

    feat(EST-2922-FE): shared ui and utils for agents page

--- frontend/src/app/shared/directives/index.ts
+++ frontend/src/app/shared/directives/index.ts
@@ -1,4 +1,7 @@
 export * from './click-outside.directive';
+export * from './collapse-on-overflow.directive';
+export * from './enter-blur.directive';
 export * from './has-permission.directive';
+export * from './hide-inline-subtitle-on-overflow.directive';
 export * from './search-shortcut.directive';
 export * from './server-errors.directive';
