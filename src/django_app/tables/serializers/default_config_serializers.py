eaf4b4f635463224d823d877e289909876d20601
OlgaMageriaHYS <olga.mageria@hys-enterprise.com>
2026-07-06 18:52:26 +0300

    fix(EST-3148-BE): remove unused import

--- src/django_app/tables/serializers/default_config_serializers.py
+++ src/django_app/tables/serializers/default_config_serializers.py
@@ -3,7 +3,6 @@ from rest_framework import serializers
 from tables.models import (
     DefaultModels,
 )
-from tables.models.realtime_models import DefaultRealtimeAgentConfig
 
 
 class DefaultModelsSerializer(serializers.ModelSerializer):
