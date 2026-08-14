56f58c26c5730d581394b76a3ba239de787bea3a
BogdanTimchenkoHYS <bogdan-timchenko@hys-enterprise.com>
2026-08-14 14:58:02 +0300

    ref(EST-3691-BE): remove Redis pub/sub handlers

--- src/knowledge_new/bootstrap/lifespans.py
+++ src/knowledge_new/bootstrap/lifespans.py
@@ -5,8 +5,6 @@ from concurrent.futures import ProcessPoolExecutor
 from typing import Literal
 
 from infrastructure.processing_run import set_process_pool
-from presentation.handlers import IndexHandler, PrechunkHandler, SearchHandler
-from presentation.handlers.cancel_handler import CancelHandler
 from settings import settings
 from src.shared.communication import Consumer, Producer, brokers, storages
 
@@ -34,23 +32,3 @@ def on_shutdown(fn: Callable):
 def init_process_pool():
     process_pool = ProcessPoolExecutor(settings.MAX_PROCESS_WORKERS)
     set_process_pool(process_pool)
-
-
-@on_startup
-async def start_handlers():
-    broker = brokers.RedisPubSubBroker(settings.BROKER_DNS)
-    storage = storages.RedisStorage(settings.STORAGE_DNS)
-    producer = Producer(broker, storage)
-    consumer = Consumer(broker, storage)
-    handlers = [PrechunkHandler, IndexHandler, SearchHandler, CancelHandler]
-    for handler in handlers:
-        task = asyncio.create_task(handler(producer, consumer).run())
-        _handler_tasks.add(task)
-        task.add_done_callback(_handler_tasks.discard)
-
-
-@on_shutdown
-async def shut_handlers():
-    for task in _handler_tasks:
-        task.cancel()
-    await asyncio.gather(*_handler_tasks)
