35e3a172a079c151051c431e070438e78cbe8b5d
OlgaMageriaHYS <olga.mageria@hys-enterprise.com>
2026-08-17 11:14:06 +0300

    fix(EST-3748-BE): fix crew crash

--- src/crew/services/run_python_code_service.py
+++ src/crew/services/run_python_code_service.py
@@ -48,11 +48,8 @@ class RunPythonCodeService(metaclass=SingletonMeta):
             storage_allowed_paths=python_code_data.storage_allowed_paths,
             storage_org_prefix=python_code_data.storage_org_prefix,
             session_id=python_code_data.session_id,
-<<<<<<< HEAD
             secrets=python_code_data.secrets,
-=======
             org_id=python_code_data.org_id,
->>>>>>> main
         )
         callback_receiver = RunPythonCallbackReceiver(execution_id=unique_task_id)
 
