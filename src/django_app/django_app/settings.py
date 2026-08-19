a277fd9daf189719678c3cbb4e64b870d533966e
VitaliiPovoroznykHYS <vitalii.povoroznyk@hys-enterprise.com>
2026-08-19 19:18:37 +0300

    fix: set page size to 50 for rest framework settings

--- src/django_app/django_app/settings.py
+++ src/django_app/django_app/settings.py
@@ -143,7 +143,7 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 500000,
+    "PAGE_SIZE": 50,
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "EXCEPTION_HANDLER": "utils.exception_handler.custom_exception_handler",
     "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
