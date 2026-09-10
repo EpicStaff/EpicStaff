c3fd4f590d105eef9d56911d57407b50db0b1ae1
BogdanTimchenkoHYS <bogdan.timchenko@hys-enterprise.com>
2026-09-10 16:43:48 +0300

    chore(EST-2124-BE): remove old knowledge

--- src/knowledge/chunkers/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base_chunker import BaseChunker, BaseChunkData
-from .character_chunker import CharacterChunker
-from .csv_chunker import CSVChunker
-from .html_chunker import HTMLChunker
-from .json_chunker import JSONChunker
-from .markdown_chunker import MarkdownChunker
-from .token_chunker import TokenChunker
